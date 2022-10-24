879785126ef00d6cfa1d038a0516d96bf8f51148
Raul Arias <raularias24685@gmail.com>
2022-10-24 18:39:37 -0400

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const menuEmail = document.querySelector('.navbarEmail')
-const desktopMenu = document.querySelector('.desktopMenu')
+const menuEmail = document.querySelector('.navbarEmail');
+const desktopMenu = document.querySelector('.desktopMenu');
+const burgerMenu = document.querySelector('.burgerMenu');
+const mobileMenu = document.querySelector('.mobileMenu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive') // toggle = El funcionamiento de toggle es cambiar en este caso la clase, la quita si la tiene puesta cuando hagamos click, o la agrega si no la tiene cuando hagamos click.
+    desktopMenu.classList.toggle('inactive'); // toggle = El funcionamiento de toggle es cambiar en este caso la clase, la quita si la tiene puesta cuando hagamos click, o la agrega si no la tiene cuando hagamos click.
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
