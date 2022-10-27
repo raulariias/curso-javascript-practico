4cef2d1ac22032ee11dc7e4e6b6b19e99d5e0e68
Raul Arias <raularias24685@gmail.com>
2022-10-26 20:12:36 -0400

    Lista de productos a partir de un Array

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const desktopMenu = document.querySelector('.desktopMenu');
 const mobileMenu = document.querySelector('.mobileMenu');
 const shoppingCart = document.querySelector('.productDetails')
+const cardsContainer = document.querySelector('.cardsContainer') // Clase para iterar array de productos
 
 /* Botones */
 const menuEmail = document.querySelector('.navbarEmail');
@@ -43,3 +44,92 @@ function toggleMyOrderMenu() {
     }
     shoppingCart.classList.toggle('inactive');
 }
+
+
+// Lista de productos a partir de un array
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productsList.push({
+    name: 'Car',
+    price: 4500,
+    image: 'https://cnnespanol.cnn.com/wp-content/uploads/2021/11/210922163312-04-lucid-air-electric-cars-full-169.jpg?quality=100&strip=info',
+})
+productsList.push({
+    name: 'PC',
+    price: 1200,
+    image: 'https://blog.seccionamarilla.com.mx/wp-content/uploads/2020/06/pc-gamer-barata-partes-y-ycaracteristicas-770x578.jpg',
+})
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productsList.push({
+    name: 'Car',
+    price: 4500,
+    image: 'https://cnnespanol.cnn.com/wp-content/uploads/2021/11/210922163312-04-lucid-air-electric-cars-full-169.jpg?quality=100&strip=info',
+})
+productsList.push({
+    name: 'PC',
+    price: 1200,
+    image: 'https://blog.seccionamarilla.com.mx/wp-content/uploads/2020/06/pc-gamer-barata-partes-y-ycaracteristicas-770x578.jpg',
+})
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productsList.push({
+    name: 'Car',
+    price: 4500,
+    image: 'https://cnnespanol.cnn.com/wp-content/uploads/2021/11/210922163312-04-lucid-air-electric-cars-full-169.jpg?quality=100&strip=info',
+})
+productsList.push({
+    name: 'PC',
+    price: 1200,
+    image: 'https://blog.seccionamarilla.com.mx/wp-content/uploads/2020/06/pc-gamer-barata-partes-y-ycaracteristicas-770x578.jpg',
+})
+
+function renderProducts(arr) {
+    for (const product of productsList) {
+        // Creando las variables y agregandole los atributos/clases necesarias.
+    
+        const productCard = document.createElement('div');
+        productCard.classList.add('productCard');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('productInfo');
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const price = document.createElement('p')
+        price.innerHTML = '$' + product.price
+    
+        const name = document.createElement('p')
+        name.innerHTML = product.name;
+    
+        const productInfoFigure = document.createElement('figure')
+    
+        const figureImg = document.createElement('img')
+        figureImg.setAttribute('src', './assets/Icons/bt_add_to_cart.svg')
+    
+        // Iterando HTML
+    
+        productInfoFigure.appendChild(figureImg)
+        productInfoDiv.append(price, name)
+        productInfo.append(productInfoDiv, productInfoFigure)
+        productCard.append(productImg, productInfo)
+    
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productsList);
